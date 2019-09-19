723fe69814b0dc0a055093a2c2cb3022ec5b33fa
Jordan Davila <hi@jordandavila.com>
2019-09-19 01:54:23 -0400

    Initial Commit w/ Main Menu Styled

---
+++ index.php
@@ -0,0 +1,81 @@
+<?php require 'library/layouts/_header.php'; ?>
+<header>
+	<nav class="main-menu">
+		<section class="container layout">
+			<a href="./index.php" class="logo"></a>
+			<section class="links">
+				<div class="link with-submenu">
+					<a href="">Solutions <i class="icon-arrow-down"></i><i class="menu-select-carrot"></i></a>
+					<div class="submenu">
+						<a href="" class="options">
+							<div class="icon for-hosp"></div>
+							<h4>For Hospitals</h4>
+							<p>FieldMed-powered community health programs reduce MICU calls by 55%</p>
+						</a>
+						<a href="" class="options">
+							<div class="icon for-comm"></div>
+							<h4>For Communities</h4>
+							<p>FieldMed-powered community health programs reduce MICU calls by 55%</p>
+						</a>
+						<a href="" class="options">
+							<div class="icon for-ems"></div>
+							<h4>For EMS</h4>
+							<p>FieldMed-powered community health programs reduce MICU calls by 55%</p>
+						</a>
+					</div>
+				</div>
+				<div class="link">
+					<a href="">Pricing<i class="menu-select-carrot"></i></a>
+				</div>
+				<div class="link">
+					<a href="">Community Health<i class="menu-select-carrot"></i></a>
+				</div>
+				<div class="link with-submenu">
+					<a href="">About Us <i class="icon-arrow-down"></i><i class="menu-select-carrot"></i></a>
+					<div class="submenu">
+						<a href="" class="options">
+							<div class="icon for-hosp"></div>
+							<h4>For Hospitals</h4>
+							<p>FieldMed-powered community health programs reduce MICU calls by 55%</p>
+						</a>
+						<a href="" class="options">
+							<div class="icon for-comm"></div>
+							<h4>For Communities</h4>
+							<p>FieldMed-powered community health programs reduce MICU calls by 55%</p>
+						</a>
+						<a href="" class="options">
+							<div class="icon for-ems"></div>
+							<h4>For EMS</h4>
+							<p>FieldMed-powered community health programs reduce MICU calls by 55%</p>
+						</a>
+					</div>
+				</div>
+				<div class="link">
+					<a href="">Blog<i class="menu-select-carrot"></i></a>
+				</div>
+				<div class="link">
+					<a href="">Contact<i class="menu-select-carrot"></i></a>
+				</div>
+				<div class="link">
+					<a href="">Solutions<i class="menu-select-carrot"></i></a>
+				</div>
+			</section>
+			<a href="" class="button-bordered-sky-blue">
+				Log In <i class="icon-arrow-right"></i>
+			</a>
+		</section>
+	</nav>
+	<section class="landing">
+
+	</section>
+	<section class="dashboard">
+
+	</section>
+</header>
+<main>
+
+</main>
+<footer>
+
+</footer>
+<?php require 'library/layouts/_footer.php'; ?>
---
+++ library/layouts/_header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>FieldMed :: Home</title>
+	<meta name="description" content="FieldMed Home Website">
+	<meta name="author" content="SitePoint">
+	<link href="/css/index.css" media="screen, projection" rel="stylesheet" type="text/css" />
+</head>
+<body>
